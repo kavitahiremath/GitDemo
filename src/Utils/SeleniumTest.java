41c89c90cd6c0e2821d3cd4ff10b228e33e92c41
kavitahiremath <kavitas.khedalkar@gmail.com>
2022-10-19 13:02:13 +0530

    final commit by gitx

---
+++ src/Utils/SeleniumTest.java
@@ -0,0 +1,15 @@
+package Utils;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	  @Test
+	  public void selnium1() {
+		  System.out.println("First method");
+		  System.out.println("Second method");
+		  System.out.println("Third method");
+		  System.out.println("Fourth method");
+		  System.out.println("Fifth method");
+	  }
+}
