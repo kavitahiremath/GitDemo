de80740f643d7ea63a2444ef4470d560d31d785b
kavitahiremath <kavitas.khedalkar@gmail.com>
2022-10-19 12:40:17 +0530

    new mobile testing

--- src/Utils/AppUtils.java
+++ src/Utils/AppUtils.java
@@ -13,6 +13,22 @@ public class AppUtils {
 	  System.out.println("Fourth method");
 	  System.out.println("Fifth method");
   }
+  @Test
+  public void f1() {
+	  System.out.println("First method");
+	  System.out.println("Second method");
+	  System.out.println("Third method");
+	  System.out.println("Fourth method");
+	  System.out.println("Fifth method");
+  }
+  @Test
+  public void f2() {
+	  System.out.println("First method");
+	  System.out.println("Second method");
+	  System.out.println("Third method");
+	  System.out.println("Fourth method");
+	  System.out.println("Fifth method");
+  }
   @BeforeMethod
   public void beforeMethod() {
 	  System.out.println("This is Start Session");
---
+++ src/Utils/MobileTesting.java
@@ -0,0 +1,41 @@
+package Utils;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class MobileTesting {
+	  @Test
+	  public void mob1() {
+		  System.out.println("First method");
+		  System.out.println("Second method");
+		  System.out.println("Third method");
+		  System.out.println("Fourth method");
+		  System.out.println("Fifth method");
+	  }
+	  @Test
+	  public void mob2() {
+		  System.out.println("First method");
+		  System.out.println("Second method");
+		  System.out.println("Third method");
+		  System.out.println("Fourth method");
+		  System.out.println("Fifth method");
+	  }
+	  @Test
+	  public void mob3() {
+		  System.out.println("First method");
+		  System.out.println("Second method");
+		  System.out.println("Third method");
+		  System.out.println("Fourth method");
+		  System.out.println("Fifth method");
+	  }
+	  @BeforeMethod
+	  public void beforeMethod() {
+		  System.out.println("This is Start Session");
+	  }
+
+	  @AfterMethod
+	  public void afterMethod() {
+		  System.out.println("This is Close Session");
+	  }
+}
